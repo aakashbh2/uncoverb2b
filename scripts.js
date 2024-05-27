f896264120682f1d1c09fbdac4b987c72640e5fa
Aakash Bhatnagar <aakashbhatnagar@Aakashs-MacBook-Air.local>
2024-05-28 00:28:57 +0530

    Major changes

--- scripts.js
+++ scripts.js
@@ -1,42 +1,14 @@
-// const carousel = document.querySelector('.carousel');
-// const prevButton = document.querySelector('.prev');
-// const nextButton = document.querySelector('.next');
-// const carouselItems = document.querySelectorAll('.carousel-item');
-// let scrollAmount = 0;
-// const scrollStep = carousel.clientWidth;
-// const scrollInterval = 10000; // 10 seconds
+document.getElementById('route-why-us').addEventListener('click', function() {
+    document.getElementById('why-us').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
 
-// function scrollNext() {
-//     if (scrollAmount >= (carouselItems.length - 1) * scrollStep) {
-//         scrollAmount = 0; // Reset to first item if end is reached
-//     } else {
-//         scrollAmount += scrollStep;
-//     }
-//     carousel.scrollTo({
-//         top: 0,
-//         left: scrollAmount,
-//         behavior: 'smooth'
-//     });
-// }
-
-// function scrollPrev() {
-//     if (scrollAmount <= 0) {
-//         scrollAmount = (carouselItems.length - 1) * scrollStep; // Go to last item if start is reached
-//     } else {
-//         scrollAmount -= scrollStep;
-//     }
-//     carousel.scrollTo({
-//         top: 0,
-//         left: scrollAmount,
-//         behavior: 'smooth'
-//     });
-// }
-
-// nextButton.addEventListener('click', scrollNext);
-// prevButton.addEventListener('click', scrollPrev);
-
-// // Auto-scroll every 10 seconds
-// setInterval(scrollNext, scrollInterval);
+document.getElementById('route-how-it-works').addEventListener('click', function() {
+    document.getElementById('how-it-works').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
 
 document.addEventListener('DOMContentLoaded', function () {
     var modal = document.getElementById("myModal");
